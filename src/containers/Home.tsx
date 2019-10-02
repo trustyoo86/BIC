f434c6f3d8ef0ae66938afe360892b462b4b0a26
유한승 <kern86@wemakeprice.com>
2019-10-02 17:22:06 +0900

    feat(DAY-3): Add Design component
    1. Add shoutem ui

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,24 +1,40 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Link } from 'react-router-native';
+import { NavigationBar, ImageBackground, Title, Icon } from '@shoutem/ui';
 
 const Home = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Link to='/map'>
-        <Text>Move to Map</Text>
-      </Link>
-    </View>
+    <React.Fragment>
+      <ImageBackground
+        source={{uri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-3.png'}}
+        style={{ width: '100%', height: 70 }}
+      >
+        <NavigationBar
+          styleName='clear'
+          leftComponent={<Icon name='sidebar'/>}
+          centerComponent={<Title>메인</Title>}
+        />
+      </ImageBackground>
+    </React.Fragment>
   );
 };
+
+// const Home = () => {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.tsx to start working on your app!</Text>
+//       <Link to='/map'>
+//         <Text>Move to Map</Text>
+//       </Link>
+//     </View>
+//   );
+// };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#c6c6c6',
   },
 });
 
--- src/containers/Map.tsx
+++ src/containers/Map.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { Link } from 'react-router-native';
 
 const Map = () => {
@@ -13,13 +13,13 @@ const Map = () => {
   );
 };
 
-const styles = {
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-};
+});
 
 export default Map;
